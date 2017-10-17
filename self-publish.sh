9dffba6c65d5694300c7ce2e3c340f838eef6dc5
Daniel Olasky <daniel.olasky@hotschedules.com>
2017-10-17 15:57:43 -0700

    updates self-publish

--- self-publish.sh
+++ self-publish.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+case $1 in 
+    patch|major|minor) echo is valid ;;
+    *) 
+        echo 'required argument patch major or minor'
+        exit 1
+        ;;
+esac
 
+ready=$(git status --porcelain)
+
+if [ -z "$ready" ]
+then
+    yarn publish --new-version $1
+else
+    echo 'git status not ready for publish'
+    exit 1
+fi
